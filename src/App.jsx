d872ea193551ad17b3bb5e966c0b273c22a421c3
MEHEDI HASAN <mehedialhasan1999@gmail.com>
2025-05-01 09:34:08 +0600

    added bg radiant color

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import "./App.css";
+import Header from "./components/Header";
 
 function App() {
 
   return (
     <>
-      <h1 class="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <div className="bg-gradient-to-r from-[rgba(103,151,255,0.4)] to-[rgba(255,111,185,0.1)] backdrop-blur-3xl">
+        <div className="px-[240px]">
+          <Header />
+        </div>
+      </div >
     </>
   )
 }
